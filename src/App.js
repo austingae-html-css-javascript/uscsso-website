ad4d967e151d05bf021d703483dd7a534665db0a
Austin Gae <austingae@gmail.com>
2022-06-20 14:54:25 -0700

    Completed Hamburger Menu. If the hamburger-menu-button clicked, then menu shows up.

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import {BrowserRouter as Router, Routes, Route, Link} from "react-router-dom";
 /*FontAwesome*/
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars } from '@fortawesome/free-solid-svg-icons';
+import { faXmark } from "@fortawesome/free-solid-svg-icons";
 import { faFacebook, faLinkedin, faInstagram, faTwitter } from '@fortawesome/free-brands-svg-icons';
 
 /*App.css*/
@@ -31,17 +32,33 @@ function App() {
           <div className='nav-bar__right-container'>
             <button className='nav-bar__hamburger-menu-button' onClick={() => {
               document.getElementsByTagName("menu")[0].style.cssText = "left: 0%; transition-property: left; transition-duration: 1s;";
-            }}><FontAwesomeIcon className='nav-bar__hamburger-menu-icon' icon={faBars} size="2x" /></button>
+            }}><FontAwesomeIcon className='nav-bar__hamburger-menu-icon' icon={ faBars } size="2x" /></button>
           </div>
         </div>
       </nav>
 
       <menu>
         <div className='menu-container'>
-          <Link className='menu__link' to="/" onClick={() => {document.getElementsByTagName("menu")[0].style.cssText = "left: -100%; transition-property: left; transition-duration: 1s;";}}>Home</Link>
-          <Link className='menu__link' to="/events" onClick={() => {document.getElementsByTagName("menu")[0].style.cssText = "left: -100%; transition-property: left; transition-duration: 1s;";}}>Events</Link>
-          <Link className='menu__link' to="/our-work" onClick={() => {document.getElementsByTagName("menu")[0].style.cssText = "left: -100%; transition-property: left; transition-duration: 1s;";}}>Our Work</Link>
-          <Link className='menu__link' to="/about" onClick={() => {document.getElementsByTagName("menu")[0].style.cssText = "left: -100%; transition-property: left; transition-duration: 1s;";}}>About</Link>
+          <button className='menu-container__x-mark-button' onClick={() => {document.getElementsByTagName("menu")[0].style.cssText = "left: -100%; transition-property: left; transition-duration: 1s;";}}><FontAwesomeIcon className='menu-container__x-mark-icon' icon={ faXmark } size="2x" /></button>
+
+          <div className="menu-container__main-body">
+            <div className="menu-container__links-container">
+              <Link className='menu-container__link' to="/" onClick={() => {document.getElementsByTagName("menu")[0].style.cssText = "left: -100%; transition-property: left; transition-duration: 1s;";}}>Home</Link>
+              <Link className='menu-container__link' to="/events" onClick={() => {document.getElementsByTagName("menu")[0].style.cssText = "left: -100%; transition-property: left; transition-duration: 1s;";}}>Events</Link>
+              <Link className='menu-container__link' to="/our-work" onClick={() => {document.getElementsByTagName("menu")[0].style.cssText = "left: -100%; transition-property: left; transition-duration: 1s;";}}>Our Work</Link>
+              <Link className='menu-container__link' to="/about" onClick={() => {document.getElementsByTagName("menu")[0].style.cssText = "left: -100%; transition-property: left; transition-duration: 1s;";}}>About</Link>
+            </div>
+
+            <div className="menu-container__social-media-container">
+             <a className='menu-container--margin-right' href="https://www.facebook.com/USCSSO"><FontAwesomeIcon className="menu-container__social-media-icon menu-container__facebook-icon" icon={ faFacebook } size="2x" /></a>
+             <a className='menu-container--margin-right' href="https://www.linkedin.com/company/uscssogwu/"><FontAwesomeIcon className="menu-container__social-media-icon menu-container__linkedin-icon" icon={ faLinkedin } size="2x" /></a>
+             <a className='menu-container--margin-right' href="https://www.instagram.com/uschinasso/"><FontAwesomeIcon className="menu-container__social-media-icon menu-container__instagram-icon" icon={ faInstagram } size="2x" /></a>
+             <a href ="https://twitter.com/uscsso"><FontAwesomeIcon className="menu-container__social-media-icon menu-container__twitter-icon" icon={ faTwitter } size="2x" /></a>
+            </div>
+          </div>
+          
+
+         
         </div>
       </menu>
 
@@ -107,4 +124,49 @@ FontAwesome Download Process:
 5) import { faFacebook } from '@fortawesome/free-brands-svg-icons';
 
 Styling with FontAwesome: https://fontawesome.com/docs/web/use-with/react/style
+*/
+
+
+/*
+If Hamburger-Menu Button clicked, show menu.
+1. Mobile First Approach
+2 and 3. Create the HTML layout and elements
+<button className='nav-bar__hamburger-menu-button'><FontAwesomeIcon className='nav-bar__hamburger-menu-icon' icon={faBars} size="2x" /></button>
+
+<menu>
+  <div className='menu-container'>
+    <Link className='menu__link' to="/" >Home</Link>
+    <Link className='menu__link' to="/events">Events</Link>
+    <Link className='menu__link' to="/our-work">Our Work</Link>
+    <Link className='menu__link' to="/about">About</Link>
+  </div>
+</menu>
+
+3. Decorate the HTML elements in CSS
+
+menu {
+  position: fixed;
+  width: 100%;
+  height: 100vh;
+
+  //Before the .nav-bar__hamburger-menu-button clicked, left: -100%. Hence, menu not shown on the screen.
+  //After the .nav-bar__hamburger-menu-button clicked, left: 0%. Hence, menu shown on the screen.
+  left: -100%; 
+}
+
+4. Add onClick to hamburger-menu-button
+
+<button className='nav-bar__hamburger-menu-button' onClick={() => {
+              document.getElementsByTagName("menu")[0].style.cssText = "left: 0%; transition-property: left; transition-duration: 1s;";
+            }}><FontAwesomeIcon className='nav-bar__hamburger-menu-icon' icon={faBars} size="2x" /></button>
+
+5. Add onClick to <Link>
+      <menu>
+        <div className='menu-container'>
+          <Link className='menu__link' to="/" onClick={() => {document.getElementsByTagName("menu")[0].style.cssText = "left: -100%; transition-property: left; transition-duration: 1s;";}}>Home</Link>
+          <Link className='menu__link' to="/events" onClick={() => {document.getElementsByTagName("menu")[0].style.cssText = "left: -100%; transition-property: left; transition-duration: 1s;";}}>Events</Link>
+          <Link className='menu__link' to="/our-work" onClick={() => {document.getElementsByTagName("menu")[0].style.cssText = "left: -100%; transition-property: left; transition-duration: 1s;";}}>Our Work</Link>
+          <Link className='menu__link' to="/about" onClick={() => {document.getElementsByTagName("menu")[0].style.cssText = "left: -100%; transition-property: left; transition-duration: 1s;";}}>About</Link>
+        </div>
+      </menu>
 */
