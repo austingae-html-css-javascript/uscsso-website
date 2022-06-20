da8348662a3e8869183e67ebf44416283ee04e08
Austin Gae <austingae@gmail.com>
2022-06-20 16:23:25 -0700

    Completed 1) Mobile First Approach 2) create the HTML layout 3) create HTML element 4) decorate HTML elements in CSS

--- src/Pages/OurWork.js
+++ src/Pages/OurWork.js
@@ -1,8 +1,18 @@
 import React from 'react'
+import "../Global.css";
+import "./OurWork.css";
 
 const OurWork = () => {
   return (
-    <div>OurWork</div>
+    <main>
+      <div className='our-work'>
+        <h1 className='our-work__title'>Take a look at our Articles and Projects!</h1>
+        <div className='our-work__articles-projects-container'>
+          <button className='our-work__articles-button our-work__articles-button--margin-right'>Articles</button>
+          <button className='our-work__projects-button'>Projects</button>
+        </div>
+      </div>
+    </main>
   )
 }
 
