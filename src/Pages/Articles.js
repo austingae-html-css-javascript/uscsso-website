6e42866f93cc927375a5ee7f76c2556dcc3a788b
Austin Gae <austingae@gmail.com>
2022-06-20 18:19:48 -0700

    I created ArticleTemplate Component. Each article on Article.js will be displayed with one ArticleTemplate Component

--- src/Pages/Articles.js
+++ src/Pages/Articles.js
@@ -1,31 +1,119 @@
 import React from 'react'
-import ImageTen from "../Images/OurWorkImages/us-china-russia-ukraine-war.webp";
-import AnneLee from "../Images/Contributors/anne-lee.webp";
 
 import "../Global.css";
 import "./Articles.css";
+
+import ArticleTemplate from './ArticleTemplate';
+
+/*Article Images*/
+import ImageTen from "../Images/OurWorkImages/us-china-russia-ukraine-war.webp";
+import ImageNine from "../Images/OurWorkImages/energy-planet.webp";
+import ImageEight from "../Images/OurWorkImages/politics-of-energy.webp";
+import ImageSeven from "../Images/OurWorkImages/chinese-military.webp";
+import ImageSix from "../Images/OurWorkImages/trump.webp";
+import ImageFive from "../Images/OurWorkImages/xi.webp";
+import ImageFour from "../Images/OurWorkImages/tsmc.webp";
+import ImageThree from "../Images/OurWorkImages/huawei.webp";
+import ImageOne from "../Images/OurWorkImages/uyghur.webp";
+
+/*Contributor Profile Picture*/
+import USCSSO from "../Images/uscsso-logo.png";
+import AnneLee from "../Images/Contributors/anne-lee.webp";
+import DominicNozzarella from "../Images/Contributors/dominic-nozzarella.webp";
+import AndrewJHarding from "../Images/Contributors/andrew-j-harding.webp";
+import NicoleWei from "../Images/Contributors/nicole-wei.webp"
+import DylanShepherd from "../Images/Contributors/dylan-shepherd.webp"
+import EmilyZimmerman from "../Images/Contributors/emily-zimmerman.webp"
 
 const Articles = () => {
   return (
     <main>
       <div className='article-container'>
-        <div className='article'> {/*One Article*/}
-          <div className='article__left-container'>
-            <img className='article__image' src={ImageTen} />
-          </div>
+        <ArticleTemplate 
+          articleImage={ImageTen}
+          articleAuthorPicture={AnneLee} 
+          articleAuthor="Anne Lee" 
+          articleDate="March 29" 
+          articleMinToRead="6 Min"
+          articleTitle="The Responses of US and China towards the Russia-Ukraine War: An Overview"
+          articleShortDescription="A residential building destroyed by shelling in the settlement of Borodyanka in the Kyiv region..."
+        /> {/*ArticleTemplate runs, which returns ArticleTemplate Lines 8-24*/}
 
-          <div className='article__right-container'>
-            <div className='article__image-container'>
-              <img className='article__author-image' src={AnneLee} />
-            </div>
-            <div>
-              <p className='article__author-name'>Anne Lee</p>
-              <p className='article__date-and-min-to-read'><span>March 29</span> * <span>6 min</span></p>
-              <h2 className='article__title'>The Responses of US and China towards the Russia-Ukraine War: An Overview</h2>
-              <p className='article__short-description'>A residential building destroyed by shelling in the settlement of Borodyanka in the Kyiv region...</p>
-            </div> 
-          </div>
-        </div>
+        <ArticleTemplate 
+          articleImage={ImageNine}
+          articleAuthorPicture={DominicNozzarella} 
+          articleAuthor="Dominic Nozzarella" 
+          articleDate="Mar 9" 
+          articleMinToRead="2 Min"
+          articleTitle="Beijing's Commitment to the Planet"
+          articleShortDescription="China has taken several steps to tackle climate change, but China’s status as the world’s largest carbon emitter..."
+        />
+
+        <ArticleTemplate 
+            articleImage={ImageEight}
+            articleAuthorPicture={AndrewJHarding} 
+            articleAuthor="Andrew J Harding" 
+            articleDate="Mar 9" 
+            articleMinToRead="2 Min"
+            articleTitle="The Politics of Energy: The BRI's Role in Energy Development"
+            articleShortDescription="As China continues to rise, so does its energy usage, dependencies, and vulnerabilities. These complicated relationships are..."
+        />
+
+        <ArticleTemplate 
+            articleImage={ImageSeven}
+            articleAuthorPicture={AndrewJHarding} 
+            articleAuthor="Andrew J Harding" 
+            articleDate="Feb 24" 
+            articleMinToRead="2 Min"
+            articleTitle="Annual Pentagon Report Confirms Notable Chinese Military Developments"
+            articleShortDescription="As the United States continues to keep a close eye on China’s military, the Pentagon released its annual report to Congress on China’s military and security..."
+        />        
+        <ArticleTemplate 
+          articleImage={ImageSix}
+          articleAuthorPicture={NicoleWei} 
+          articleAuthor="Nicole Wei" 
+          articleDate="Jan 29" 
+          articleMinToRead="4 Min"
+          articleTitle="This Day in History: January 15, 2020 - Phase One Trade Deal Agreement"
+          articleShortDescription='On January 15, 2020…then-US President Donald Trump and China’s Vice Premier Liu He signed a “historic, enforceable agreement"...'
+        />
+        <ArticleTemplate 
+          articleImage={ImageFive}
+          articleAuthorPicture={DylanShepherd} 
+          articleAuthor="Dylan Shepherd" 
+          articleDate="Dec 1" 
+          articleMinToRead="14 Min"
+          articleTitle="Understanding The Changing Nature of Chinese Factional Politics"
+          articleShortDescription="Many in the West think of politics in China as a monolithic institution controlled top-down by Xi Jinping. After numerous reports..."
+        />
+        <ArticleTemplate 
+          articleImage={ImageFour}
+          articleAuthorPicture={EmilyZimmerman} 
+          articleAuthor="Emily Zimmerman" 
+          articleDate="Nov 16" 
+          articleMinToRead="8 Min"
+          articleTitle="Semiconductor Supply Chains"
+          articleShortDescription="Since its invention by engineers in the United States in the 1950s, the semiconductor chip has become a focal point of trade relations between the United States and East Asian powers..."
+        />
+        <ArticleTemplate 
+          articleImage={ImageThree}
+          articleAuthorPicture={NicoleWei} 
+          articleAuthor="Nicole Wei" 
+          articleDate="Nov 5" 
+          articleMinToRead="4 Min"
+          articleTitle="A Case of Collateral Damage: American Suppliers Impacted By Huawei Sanctions"
+          articleShortDescription="The global 5G industry is emerging as the newest technological frontier, with promises of revolutionizing communication and bringing the world closer..."
+        />
+        <ArticleTemplate 
+          articleImage={ImageOne}
+          articleAuthorPicture={USCSSO} 
+          articleAuthor="USCSSO" 
+          articleDate="March 9" 
+          articleMinToRead="2 Min"
+          articleTitle="IS THE UNITED STATES' RESPONSE TO THE UYGHUR HUMAN RIGHTS CRISIS IN CHINA APPROPRIATE?"
+          articleShortDescription="The human rights violations carried out by the Chinese government against Uyghur and other minority Muslim groups such as Kazakhs and Uzbeks in the semi-autonomous region of Xinjiang should be condemned in all..."
+        />
+
       </div>
     </main>
   )
